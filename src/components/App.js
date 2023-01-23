36b1463232ab42f5950bdaddb74e81a2b1301b8f
Abdull0084 <Abdimalik0084@gmail.com>
2023-01-23 07:31:32 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AccountContainer from "./AccountContainer";
 
+
 function App() {
   return (
     <div className="ui raised segment">
@@ -8,6 +9,7 @@ function App() {
         <h2>The Royal Bank of Flatiron</h2>
       </div>
       <AccountContainer />
+      
     </div>
   );
 }
